a67bc5ab8d3dd93fc6d583a6760fef487609a86e
Xiaoxue Zheng <xiaoxue.zheng@manchester.ac.uk>
2021-09-06 17:54:37 +0100

    Fix test caused by merge code
    Change-Id: Ia1fed3827b2221014a64ecef2198914b36abbf43

--- application/src/test/java/uk/ac/herc/bcra/testutils/StudyUtil.java
+++ application/src/test/java/uk/ac/herc/bcra/testutils/StudyUtil.java
@@ -65,12 +65,6 @@ public class StudyUtil {
         studyId.setRiskAssessmentResponse(rar);
         em.persist(studyId);
 
-        StudyId studyId = new StudyId();
-        studyId.setCode(identifier);
-        studyId.setConsentResponse(cr);
-        studyId.setRiskAssessmentResponse(rar);
-        em.persist(studyId);
-
         Participant p = new Participant();
         p.setUser(u);
         p.setIdentifiableData(id);
